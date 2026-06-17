61c177b6ecbe235e68c498718e3c78717bc4f3cf
Atharvasayyyy <atharvasable1204@gmail.com>
2026-06-17 08:11:32 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -5,16 +5,9 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const { Server } = require("socket.io");
 
-const io = new Server(server,{
-    cors:{
-        origin:"*"
-    }
-});
-
 dotenv.config();
 
 const connecttodb = require("./src/db/db");
-
 const app = require("./src/app");
 
 app.use(cors());
@@ -23,9 +16,35 @@ app.use(morgan("dev"));
 
 connecttodb();
 
+// Create HTTP Server
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+// Attach Socket.IO
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(
+        "User Connected:",
+        socket.id
+    );
+
+    socket.on(
+        "disconnect",
+        () => {
+            console.log(
+                "User Disconnected:",
+                socket.id
+            );
+        }
+    );
+});
+
+const PORT =
+    process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(
